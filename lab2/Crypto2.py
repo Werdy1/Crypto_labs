9512513b8da42cd2e04c75c263678f79b8415362
Ihor Prykhodko <94651198+Werdy1@users.noreply.github.com>
2023-05-25 16:50:16 +0300

    Making first structure

---
+++ lab2/Crypto2.py
@@ -0,0 +1,14 @@
+def pollards_p_method(): # will be copied from first lab
+    pass
+
+def horners_method(): # ? maybe will be copied from lab1, if needed
+    pass
+
+def try_method():
+    pass
+
+def silver_polig_hellman():
+    pass
+
+n = int(input("Enter a number:"))
+print(n)
