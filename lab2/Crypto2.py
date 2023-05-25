dc544c393a69d91b57c4033e9b3d6c80c905e6ad
Ihor Prykhodko <94651198+Werdy1@users.noreply.github.com>
2023-05-25 17:49:18 +0300

    Realised that I need even more functions from lab1
    Added functions: find_highest_power_of_two, miller_rabin_probability_test
    Started to do silver_polig_hellman_method (changed it's name)

--- lab2/Crypto2.py
+++ lab2/Crypto2.py
@@ -1,11 +1,48 @@
+import random
+
 def gcd(a: int,b: int):
     return abs(a) if b==0 else gcd(b, a%b)
+
+def find_highest_power_of_two(n: int):
+    # find the max 2**s that can divide number (n)
+    return (n & (~(n - 1)))
+
+def miller_rabin_probability_test(n: int): 
+    # if number(n) is prime the function will return True
+    # otherwise False
+    if n==2:
+        return True
+    power_of_two = find_highest_power_of_two(n-1)
+    s = power_of_two.bit_length()
+    d = int((n-1)/power_of_two) #for whatever reason it's not working with double, so converted to int (maybe somethig with) double representation
+    k = 5
+    for i in range(k):
+        x = random.randint(2,n-1) # both ends includes
+        if gcd(n,x) > 1:
+            return False
+        base = horners_method(x,d,n)
+        if  base == 1 or base == (n - 1):
+            continue
+        for j in range(1,s):
+            base = (base**2)%n
+            if base == n - 1:
+                break
+            elif base == 1 or j == s-1:
+                return False
+    return True
 
 def pollards_p_method(n: int): 
     # this function will find one divider and after that ends
     # I will use classic function: f(x) = x^2 + 1
     answer = [n] # list where found dividers and new n will be stored
                  # if none dividers found list with only n will be returned 
+
+    if n ==1:
+        print("Number equals 1")
+        return answer
+    if miller_rabin_probability_test(n):
+        print("Already prime")
+        return answer
     x = 2
     y = 2
     poss_divider = 1
@@ -48,7 +85,14 @@ def try_method(a: int, b:int,p: int):
 def silver_polig_hellman(a: int, b:int,p: int):
     # where  a - base (generator); b - field element; p - module (prime number)
     n = p - 1 # order of the group
+    temp = n
+    canonical_form = []
+    while temp != 1:
+        numbers = pollards_p_method(n) # to do: add handling of infinite loop, if temp is prime?
+        temp = numbers[0]
+        canonical_form.append(temp[1])
+    
     pass
 
 n = int(input("Enter numbers:"))
-print(try_method(5,11,73))
+print(try_method(2,13,37))
